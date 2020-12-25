52281aa2c99a1be275d698b6582602133aed47d2
GeneKong <gyx_edu@qq.com>
2020-12-25 09:41:29 +0800

    fixed：4.6可以干活

---
+++ include/project-conf.h
@@ -0,0 +1,3 @@
+// #define LOG_CONF_LEVEL_MAC                          LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_FRAMER                       LOG_LEVEL_DBG
+// #define LOG_CONF_LEVEL_RPL                          LOG_LEVEL_DBG
